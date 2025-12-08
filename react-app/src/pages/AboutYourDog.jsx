25cd79c6c9b60911ca55df6124865ea61c82e299
Jiasui_Qin <qjs1110@uw.edu>
2025-12-07 21:52:25 -0800

    fix bug of css

--- react-app/src/pages/AboutYourDog.jsx
+++ react-app/src/pages/AboutYourDog.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './AboutYourPet.css';
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import "./AboutYourDog.css";
 
 function AboutYourPet() {
   const navigate = useNavigate();
